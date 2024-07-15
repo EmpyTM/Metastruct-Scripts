841625570c676ef4de59d2dc150a8c1d34adb319
EmpyTM <37101108+EmpyTM@users.noreply.github.com>
2024-07-15 01:06:40 -0500

    fixed issue with tabs not converting to spaces

--- bhopbot/utils.lua
+++ bhopbot/utils.lua
@@ -1,39 +1,39 @@
 local utils = {}
 
 function utils.save( filename, positions, connections )
-	local payload = {}
-	payload.version = bhopbot.version -- ensure format compatibility
-	payload.positions = {}
-	payload.connections = {}
+    local payload = {}
+    payload.version = bhopbot.version -- ensure format compatibility
+    payload.positions = {}
+    payload.connections = {}
 
-	for position_index, position in ipairs(positions) do
-		payload.positions[position_index] = { position.x, position.y, position.z }
-	end
+    for position_index, position in ipairs(positions) do
+        payload.positions[position_index] = { position.x, position.y, position.z }
+    end
 
-	for connection_index, connection in pairs(connections) do
-		payload.connections[connection_index] = connection
-	end
+    for connection_index, connection in pairs(connections) do
+        payload.connections[connection_index] = connection
+    end
 
-	local to_json = util.tableToJSON(payload)
-	file.Write(filename, to_json)
+    local to_json = util.tableToJSON(payload)
+    file.Write(filename, to_json)
 end
 
 function utils.load( filename )
-	local json_file = file.Read(filename)
+    local json_file = file.Read(filename)
 
-	if ( not json_file ) then
-		local formatted_error = string.format("ERROR: \"%s\": bhopbot data not found or invalid!", filename)
-		error(formatted_error)
-	end
+    if ( not json_file ) then
+        local formatted_error = string.format("ERROR: \"%s\": bhopbot data not found or invalid!", filename)
+        error(formatted_error)
+    end
 
-	local dat = util.JSONToTable(json_file)
+    local dat = util.JSONToTable(json_file)
 
-	if ( not dat.version or dat.version ~= bhopbot.version ) then
-		local formatted_error = string.format("ERROR: mismatched version -- \"%s\" does not match the newest release \"%s\"", dat.version, bhopbot.version)
-		error(formatted_error)
-	end
+    if ( not dat.version or dat.version ~= bhopbot.version ) then
+        local formatted_error = string.format("ERROR: mismatched version -- \"%s\" does not match the newest release \"%s\"", dat.version, bhopbot.version)
+        error(formatted_error)
+    end
 
-	return { dat.positions, dat.connections }
+    return { dat.positions, dat.connections }
 end
 
 return utils
