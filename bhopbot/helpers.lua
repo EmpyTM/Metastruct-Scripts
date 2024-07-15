cde721f1af509d5f1ae6552410acf76658d4d339
EmpyTM <37101108+EmpyTM@users.noreply.github.com>
2024-07-15 01:03:52 -0500

    formatting fix (maybe)

--- bhopbot/helpers.lua
+++ bhopbot/helpers.lua
@@ -1,34 +1,34 @@
 local helpers = {}
 
-function helpers.get_players_sitting_on( ply )	
-	local players = player.GetAll()
-	local sitting_players = {}
-	
-	for player_index, player_entity in ipairs(players) do
-		if ( player_entity == ply ) then continue end
-		if ( not v:InVehicle() ) then continue end
+function helpers.get_players_sitting_on( ply )    
+    local players = player.GetAll()
+    local sitting_players = {}
+    
+    for player_index, player_entity in ipairs(players) do
+        if ( player_entity == ply ) then continue end
+        if ( not v:InVehicle() ) then continue end
         
         local veh = player_entity:GetVehicle()
-		if ( veh:GetParent() ~= ply ) then continue end
+        if ( veh:GetParent() ~= ply ) then continue end
         
         table.insert(sitting_players, v)
-	end
-	
-	return sitting_players
+    end
+    
+    return sitting_players
 end
 
-function helpers.find_player_by_name( desired_name )	
-	local players = player.GetAll()
+function helpers.find_player_by_name( desired_name )    
+    local players = player.GetAll()
     local ply = nil
-	for player_index, player_entity in ipairs(players) do
+    for player_index, player_entity in ipairs(players) do
         local player_name = v:GetName()
-		if ( string.find(player_name, desired_name) ) then
-			ply = player_entity
+        if ( string.find(player_name, desired_name) ) then
+            ply = player_entity
             break
-		end
-	end
+        end
+    end
 
-	return ply
+    return ply
 end
 
 return helpers
