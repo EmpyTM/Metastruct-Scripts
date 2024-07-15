2adc7d6631948d804d7c2579c416f741abaaa6c9
EmpyTM <37101108+EmpyTM@users.noreply.github.com>
2024-07-15 01:05:00 -0500

    fixed trailing whitespace

--- bhopbot/helpers.lua
+++ bhopbot/helpers.lua
@@ -1,6 +1,6 @@
 local helpers = {}
 
-function helpers.get_players_sitting_on( ply )    
+function helpers.get_players_sitting_on( ply )
     local players = player.GetAll()
     local sitting_players = {}
     
@@ -17,7 +17,7 @@ function helpers.get_players_sitting_on( ply )
     return sitting_players
 end
 
-function helpers.find_player_by_name( desired_name )    
+function helpers.find_player_by_name( desired_name )
     local players = player.GetAll()
     local ply = nil
     for player_index, player_entity in ipairs(players) do
