39e57f8170461bb1f5677311b2c369d0862157ba
EmpyTM <37101108+EmpyTM@users.noreply.github.com>
2024-08-02 16:51:47 -0500

    Update utils.lua

--- bhopbot/utils.lua
+++ bhopbot/utils.lua
@@ -1,18 +1,12 @@
 local utils = {}
 
-do
-    local err_fmt = "[BHopBot] ERROR: %s"
-    local err_len = 4
-    local snd = "buttons/button8.wav"
+function utils.error(condition, error_message)
+    if condition then
+        local err_out = string.format("[BHopBot] ERROR: %s", error_message)
+        notification.AddLegacy(err_out, NOTIFY_ERROR, 4)
+        surface.PlaySound("buttons/button8.wav")
 
-    function utils.error(condition, error_message)
-        if (condition) then
-            local err_out = string.format(err_fmt, error_message)
-            notification.AddLegacy(err_out, NOTIFY_ERROR, err_len)
-            surface.PlaySound(snd)
-
-            return
-        end
+        return
     end
 end
 
@@ -23,39 +17,24 @@ function utils.save(filename, positions, connections)
     payload.connections = {}
 
     for position_index, position in ipairs(positions) do
-        local t = {}
-        t[1] = position.x
-        t[2] = position.y
-        t[3] = position.z
-        payload.positions[position_index] = t
+        payload.positions[position_index] = {position.x, position.y, position.z}
     end
 
     for connection_index, connection in pairs(connections) do
         payload.connections[connection_index] = connection
     end
 
-    local to_json = util.tableToJSON(payload)
-    file.Write(filename, to_json)
+    file.Write(filename, util.tableToJSON(payload))
 end
 
 function utils.load(filename)
     local json_file = file.Read(filename)
-    
-    local json_file_err_out = string.format("\"%s\": bhopbot data not found or invalid!", filename)
-    utils.error(not json_file, json_file_err_out)
+    utils.error(not json_file, ("\"%s\": bhopbot data not found or invalid!"):format(filename))
 
     local dat = util.JSONToTable(json_file)
+    utils.error(not dat.version or dat.version ~= bhopbot.version, ("mismatched version \"%s\" does not match the newest release \"%s\""):format(dat.version, bhopbot.version))
 
-    local current_version = dat.version
-    local version_err_out = string.format("mismatched version \"%s\" does not match the newest release \"%s\"",
-        current_version, bhopbot.version)
-    utils.error(not current_version or current_version ~= bhopbot.version, version_err_out)
-
-    local ret = {}
-    ret.positions = dat.positions
-    ret.connections = dat.connections
-
-    return ret
+    return {positions = positions, connections = connections}
 end
 
 return utils
