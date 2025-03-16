95732db841697409d58a956979dd7c7aab7bb676
EmpyTM <37101108+EmpyTM@users.noreply.github.com>
2025-03-15 23:04:38 -0500

    minor changes

--- bhopbot/helpers.lua
+++ bhopbot/helpers.lua
@@ -5,14 +5,14 @@ function helpers.get_players_sitting_on(target_player)
     local sitting_players = {}
     
     for player_index, player_entity in ipairs(players) do
-        if player_entity == target_player then continue end
+        if (player_entity == target_player) then continue end
         
         local in_vehicle = player_entity:InVehicle()
-        if not in_vehicle then continue end
+        if (not in_vehicle) then continue end
 
         local veh = player_entity:GetVehicle()
         local veh_parent = veh:GetParent()
-        if veh_parent ~= target_player then continue end
+        if (veh_parent ~= target_player) then continue end
         
         table.insert(sitting_players, player_entity)
     end
@@ -23,14 +23,14 @@ end
 function helpers.find_player_by_name(desired_name)
     local players = player.GetAll()
     local ply = nil
-    local find_start_pos = 1
-    local no_patterns = false
+    local to_find_start_position = 1
+    local use_patterns = false
 
     for player_index, player_entity in ipairs(players) do
         local player_name = player_entity:GetName()
-        local found_start, found_end, matched = string.find(player_name, desired_name, find_start_pos, no_patterns)
+        local found_start, found_end, matched = player_name:find(desired_name, to_find_start_position, use_patterns)
         
-        if found_start then
+        if (found_start) then
             ply = player_entity
             break
         end
